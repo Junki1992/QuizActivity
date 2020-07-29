3eb6314788f2242b73629000a5111734e61ec8f6
林絢己 <junki.warner@me.com>
2020-07-29 15:13:02 +0900

    ResultActivityにNEXTボタンを設けて押下すると結果が表示されると同時に結果に応じた音声を再生させる仕様にしました。

--- app/src/main/java/com/websarva/quizapplication/ResultActivity.kt
+++ app/src/main/java/com/websarva/quizapplication/ResultActivity.kt
@@ -2,6 +2,10 @@ package com.websarva.quizapplication
 
 import android.content.Intent
 import android.graphics.Color
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.SoundPool
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_result.*
@@ -9,6 +13,15 @@ import kotlinx.android.synthetic.main.activity_result.*
 class ResultActivity : AppCompatActivity() {
 
     var numberOfQuestion = 0
+    var soundID1 = 0
+    var soundID2 = 0
+
+    lateinit var soundPool: SoundPool
+
+    //play関数を拡張関数で宣言（play2）
+    fun SoundPool.play2(soundId: Int) {
+        this.play(soundId, 1.0f, 1.0f, 0, 0, 1.0f)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +38,10 @@ class ResultActivity : AppCompatActivity() {
         //正解数を表示
         textViewCorrect.text = intCorrect.toString()
 
-        //結果を判定
-        if (intCorrect >= numberOfQuestion -5) {
-            textViewResultMessage.text = "CLEAR!!"
-            textViewResultMessage.setTextColor(Color.BLUE)
-        } else {
-            textViewResultMessage.text = "FAILURE..."
-            textViewResultMessage.setTextColor(Color.RED)
+        //クリックリスナー
+        buttonNextResult.setOnClickListener {
+            //onResultの呼び出し
+            onResult()
         }
 
         //TRY AGAINボタンでMainActivityへ遷移
@@ -39,5 +49,44 @@ class ResultActivity : AppCompatActivity() {
             val intent = Intent(this@ResultActivity, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //SoundPoolクラスをインスタンス化
+        soundPool = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            SoundPool.Builder().setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .build())
+                .setMaxStreams(1)
+                .build()
+        } else {
+            SoundPool (1, AudioManager.STREAM_MUSIC, 0)
+        }
+        //音声ファイルをメモリーにロード
+        soundID1 = soundPool.load(this, R.raw.clear, 1)
+        soundID2 = soundPool.load(this, R.raw.failure, 1)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        soundPool.release()
+    }
+
+    private fun onResult() {
+        //結果を判定（合格なら青字で”CLEAR!!”、不合格なら赤字で”FAILURE...”を表示）
+        //判定結果に合わせて音声ファイルを再生
+        if (intCorrect >= numberOfQuestion -5) {
+            textViewResultMessage.text = "CLEAR!!"
+            textViewResultMessage.setTextColor(Color.BLUE)
+            soundPool.play2(soundID1)
+        } else {
+            textViewResultMessage.text = "FAILURE..."
+            textViewResultMessage.setTextColor(Color.RED)
+            soundPool.play2(soundID2)
+        }
+        //NEXTボタンを無効化
+        buttonNextResult.isEnabled = false
     }
 }
